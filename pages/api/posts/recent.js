190ba1e878af1b2521a901b65e77fb7b2221e841
Kermit2626 <oligray26@gmail.com>
2022-06-28 00:37:04 +0100

    Update recent.js

--- pages/api/posts/recent.js
+++ pages/api/posts/recent.js
@@ -1,14 +1,15 @@
 import fs from 'fs';
+import path from 'path';
 import matter from 'gray-matter';
 
 export default async function hander(req, res){
     if(req.method === 'GET'){
-        const files = fs.readdirSync('posts');
+        const files = fs.readdirSync(path.join(process.cwd(), 'posts'));
         
         // need to order these files into date released
         const recentFiles = files.slice(0, 4);
         const posts = recentFiles.map((fileName) => {
-            const readFile = fs.readFileSync(`posts/${fileName}`);
+            const readFile = fs.readFileSync(path.join(process.cwd(), 'posts', `${fileName}`));
             const { data: frontMatter } = matter(readFile);
 
             return frontMatter
