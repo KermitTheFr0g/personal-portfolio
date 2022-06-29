d4755dc4a6c870e9e984a687b4119d138dce8bf5
Oli <oligray26@gmail.com>
2022-06-29 12:44:10 +0100

    about me profile

--- components/modules/AboutMeProfile.jsx
+++ components/modules/AboutMeProfile.jsx
@@ -18,21 +18,33 @@ function AboutMeProfile(){
                     What Am I currently working on?
                 </div>
                 <p className='p-1'>
-                    what am i working on here
+                    Currently I am finishing my Cyber Security degree and I am working on various other projects on the side.
+                    I have now began this blog and my YouTube channel too which are the two platforms I am creating content for.
+                    Along with doing my own tutorials and programming projects, the major project I am working on is Dungeon Looters
+                    which is an MMO text adventure game which is interacted with using API requests. The idea behind Dungeon Looters
+                    is to be a platform for new programmers and experienced programmers to create solutions for the API or learn how to
+                    interact with an API. Apart from my own projects I am working with a small team creating NFT projects, this means I
+                    am creating sites and writing contracts in solidity. I will go into more detail on the projects I am working on in a 
+                    a blog soon.
                 </p>
 
                 <div className='pt-3 text-xl'>
-                    What am I looking to achieve in the next few years?
+                    Technologies
                 </div>
                 <p className='p-1'>
-                    what i would like to achieve
+                    I am fluent in the classic languages, like Javascript and Python. I am still looking to expand my knowledge 
+                    by learning some more languages. Along with these languages I have used ExpressJS, ReactJS, NextJS, and MySQL. All of
+                    these different technologies have different benefits and like I said I am looking to expand my knowledge more and
+                    this will all be documented here on my blog.
                 </p>
 
                 <div className='pt-3 text-xl'>
-                    How am I working to achieve this goals?
+                    When I am not coding...
                 </div>
                 <p className='p-1'>
-                    how i am going to achieve this
+                    When I am not coding or trying to break stuff I enjoy fitness and reading. You can follow my fitness journey 
+                    <a className='pl-1 text-purple-300 underline' href="https://www.instagram.com/oligray_fitness/">here</a>. If you have any books
+                    to recommend or want any tips for working out (although I am not a PT) feel free to contact me.
                 </p>
 
             </div>  
